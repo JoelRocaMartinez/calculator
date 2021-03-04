c65e9c42bf9a62f6c2cc4404f7697e722324e90b
Joel <joel.roca.martinez@hotmail.com>
2021-03-04 17:30:12 +0100

    added some functionalities

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { useState} from "react";
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(4)
+  const [count, setCount] = useState(() =>  0)
+  const [colour, setColour] = useState(() => "")
+
+
+  const decrementCount = () => {
+    setCount(prevCount => prevCount - 1)
+    console.log(count)
+    if (count <= 0) {
+      setColour("red")
+    }
+  }
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1)
+    if (count > 0) {
+      setColour("blue")
+    }
+  }
 
 
   return (
     <div>
-      <button> - </button>
+      <button onClick={decrementCount}> - </button>
       <span>{count}</span>
-      <button>+</button>
+      <span>{colour}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
   )
    
